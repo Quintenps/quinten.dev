95a8672450f75ab09929c42b237e12041cc91103
Quinten Peels <quinten@peels.me>
2024-06-15 22:19:14 +0200

    Feature/client expanded info (#4)
    * gitsave
    
    * Styling changes
    
    * changes

--- components/ComputerCard.tsx
+++ components/ComputerCard.tsx
@@ -8,23 +8,22 @@ export default function ToolCard({ computer }: { computer: Computer }) {
       <div className="flex flex-row">
         <h5 className="font-semibold">{computer.name}</h5>
         {computer.image && (
-            <a href={computer.image} className="mx-2" target="_blank">
-          <svg
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-            stroke-linejoin="round"
-            stroke-miterlimit="2"
-            height={24}
-            width={24}
-            viewBox="0 0 24 24"
-            className="fill-gray-200"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="m2 4v16.002c0 .385.22.735.567.902.346.166.758.119 1.058-.121l4.725-3.781h12.65c.552 0 1-.448 1-1v-12.002c0-.552-.448-1-1-1h-18c-.552 0-1 .448-1 1zm18.5 11.502h-12.677l-4.323 3.46v-14.462h17zm-8.502-6.5c.414 0 .75.336.75.75v3.5c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-3.5c0-.414.336-.75.75-.75zm.002-3c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1z"
-              fill-rule="nonzero"
-            />
-          </svg>
+          <a href={computer.image} className="mx-2" target="_blank">
+            <svg
+              className="w-6 h-6 text-white opacity-80 hover:opacity-100 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z"
+                clipRule="evenodd"
+              />
+            </svg>
           </a>
         )}
       </div>
